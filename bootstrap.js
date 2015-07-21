8e112ff3a6f9e9a1f3ed2ba089ebf07981f6378a
Raif Harik <reharik@gmail.com>
2015-07-21 16:55:48 -0500

    workign on bootstrap

--- bootstrap.js
+++ bootstrap.js
@@ -5,6 +5,7 @@ var _container = require('DAGon');
 module.exports = new _container(x=>
     x.pathToRoot(__dirname)
         .requireDirectoryRecursively('./src')
+        .groupAllInDirectory('./src/CommandHandlers', 'commandHandlers')
         .for('gesConnection').callInitMethod('openConnection')
         .for('gesRepository').instantiateWith({eventTypeNameHeader: 'commandTypeName'})
         .rename('lodash').withThis('_')
