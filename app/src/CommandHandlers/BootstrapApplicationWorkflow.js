c97a22efe5112c86b12305e3fd85203c42b1c45f
Raif Harik <reharik@gmail.com>
2016-02-21 19:45:46 -0600

    working on the workflow

--- app/src/CommandHandlers/BootstrapApplicationWorkflow.js
+++ app/src/CommandHandlers/BootstrapApplicationWorkflow.js
@@ -6,7 +6,7 @@
 module.exports = function(eventhandlerbase,
                           eventrepository,
                           logger,
-                          appdomain) {
+                          appdomain,co) {
 
     return class BootstrapApplicationWorkflow extends eventhandlerbase {
         constructor() {
@@ -15,18 +15,43 @@ module.exports = function(eventhandlerbase,
             this.handlerName   = 'bootstrapApplication';
         }
 
-        loginTrainer(vnt) {
+        *loginTrainer(vnt) {
             console.log(vnt);
             this.createNotification(vnt);
-            var trainer = eventrepository.getById(appdomain.Trainer, vnt.Id);
+            var trainer = yield eventrepository.getById(appdomain.Trainer, vnt.Id);
             trainer.loginTrainer(vnt);
-            eventrepository.save(trainer, {continuationId});
+            yield eventrepository.save(trainer, {continuationId});
         }
 
-        bootstrapApplication(vnt) {
+        *bootstrapApplication(vnt) {
             console.log('inside bootstrapper handler');
-            console.log(vnt);
-            //hireTrainer();
+            logger.info('calling hiretrainer');
+            var trainer = new appdomain.Trainer();
+            trainer.hireTrainer({
+                credentials: {
+                    userName: 'admin',
+                    password: '123456'
+                },
+                contact    : {
+                    firstName   : 'Raif',
+                    lastName    : 'Harik',
+                    emailAddress: 'reharik@gmail.com',
+                    phone       : '666.666.6666',
+                    secondPhone : '777.777.7777'
+                },
+                address    : {
+                    address1: '1706 willow st',
+                    address2: 'b',
+                    city    : 'Austin',
+                    state   : 'TX',
+                    zipCode : '78702'
+                }
+                ,
+                dob        : new Date()
+            });
+            logger.info('saving trainer');
+            logger.trace(trainer);
+            yield eventrepository.save(trainer);
             return 'success';
         }
 
