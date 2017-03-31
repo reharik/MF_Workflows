7b7c6ec061be9a9b1cee4ff81fafcf88748c7e73
Raif Harik <reharik@gmail.com>
2017-03-30 22:58:18 -0500

    working on event flow

--- app/src/dispatch.js
+++ app/src/dispatch.js
@@ -7,6 +7,10 @@
 module.exports = function(eventDispatcher, CommandHandlers_array, eventReceiver){
     return function(){
         var source = eventDispatcher().startDispatching('command');
+  console.log('==========eventReceiver=========');
+  console.log(eventReceiver.toString());
+  console.log('==========END eventReceiver=========');
+
         CommandHandlers_array.map(x => eventReceiver(source, x()));
     };
 };
