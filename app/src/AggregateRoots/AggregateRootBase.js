776227395660a0f40d7c32b8563e054978cbda60
Raif Harik <reharik@gmail.com>
2017-04-10 07:43:37 -0500

    adding sessions

--- app/src/AggregateRoots/AggregateRootBase.js
+++ app/src/AggregateRoots/AggregateRootBase.js
@@ -23,7 +23,7 @@ module.exports = function(invariant, logger) {
         }
 
         applyEvent(event) {
-            logger.debug(`event currently in applyEvent`);
+            logger.debug(`${event.eventName} currently in applyEvent`);
             logger.debug(JSON.stringify(event));
             var eventHandlers = this.applyEventHandlers();
             var key = Object.keys(eventHandlers).find(x => x === event.eventName );
