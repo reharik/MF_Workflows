bb44196e5dd64173779c31fda8024ee9255ce6db
Raif Harik <reharik@gmail.com>
2017-05-17 19:14:39 -0500

    fixing methods in client

--- app/src/AggregateRoots/Client.js
+++ app/src/AggregateRoots/Client.js
@@ -71,7 +71,7 @@ module.exports = function(AggregateRootBase, ClientInventory, invariant, uuid) {
           this.raiseEvent(cmd);
         }
       }
-    };
+    }
 
     applyEventHandlers() {
       return {
@@ -98,14 +98,14 @@ module.exports = function(AggregateRootBase, ClientInventory, invariant, uuid) {
       }
     }
 
-    generateSessions = (cmd) => {
+    generateSessions(cmd) {
       return [].concat(
         this.addFullHourSessions(cmd),
         this.addHalfHourSessions(cmd),
         this.addPairSessions(cmd));
     };
 
-    createNewSessionEvent = (type, purchasePrice, purchaseId) => {
+    createNewSessionEvent(type, purchasePrice, purchaseId) {
       event.eventName = `${type}SessionPurchased`;
       event.clientId = this._id;
       event.sessionId = uuid.v4();
@@ -115,7 +115,7 @@ module.exports = function(AggregateRootBase, ClientInventory, invariant, uuid) {
       return event;
     };
 
-    addFullHourSessions = (cmd) => {
+    addFullHourSessions(cmd) {
       const individualHourPrice = cmd.fullHour ? cmd.fullHourTotal / cmd.fullHour : 0;
       const tenPackHourPrice = cmd.fullHourTenPack ? cmd.fullHourTenPackTotal / (cmd.fullHourTenPack * 10) : 0;
       let sessions = Array(cmd.fullHour).fill(this.createNewSessionEvent('fullHour', individualHourPrice, cmd.id));
@@ -125,7 +125,7 @@ module.exports = function(AggregateRootBase, ClientInventory, invariant, uuid) {
       );
     };
 
-    addHalfHourSessions = (cmd) => {
+    addHalfHourSessions(cmd) {
       const individualHalfHourPrice = cmd.halfHour ? cmd.halfHourTotal / cmd.halfHour : 0;
       const tenPackHalfHourPrice = cmd.halfHourTenPack ? cmd.halfHourTenPackTotal / (cmd.halfHourTenPack * 10) : 0;
       let sessions = Array(cmd.halfHour).fill(this.createNewSessionEvent('halfHour', individualHalfHourPrice, cmd.id));
@@ -135,7 +135,7 @@ module.exports = function(AggregateRootBase, ClientInventory, invariant, uuid) {
       );
     };
 
-    addPairSessions = (cmd) => {
+    addPairSessions(cmd) {
       const individualPairPrice = cmd.pair ? cmd.pairTotal / cmd.pair : 0;
       const tenPackPairPrice = cmd.pairTenPack ? cmd.pairTenPackTotal / (cmd.pairTenPack * 10) : 0;
       let sessions = Array(cmd.pair).fill(this.createNewSessionEvent('pair', individualPairPrice, cmd.id));
